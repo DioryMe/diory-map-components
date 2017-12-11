76e1b5fc1dd29f7723c4838660de1f24788fab53
Olli-Pekka Pohjola <op@Olli-Pekkas-MacBook-Pro.local>
2017-12-11 23:46:42 +0200

    enhance diorys with map wrapper only when geo data exists

--- src/DioryGoogleMap/enhanceWithDioryMapWrapper.js
+++ src/DioryGoogleMap/enhanceWithDioryMapWrapper.js
@@ -1,10 +1,11 @@
 import React, { cloneElement } from 'react'
-import DioryMapWrapper from '../DioryMapWrapper/DioryMapWrapper'
+import { DioryMapWrapper } from '../.'
 
-export const enhanceWithDioryMapWrapper = Diory => cloneElement(<DioryMapWrapper />, {
+export const enhanceWithDioryMapWrapper = Diory => hasGeoData(Diory) && cloneElement(<DioryMapWrapper />, {
   key: Diory.key,
   lat: Diory.props.data.geo.latitude,
   lng: Diory.props.data.geo.longitude,
   Diory
 })
 
+const hasGeoData = Component => Component && Component.props && Component.props.data && Component.props.data.geo
